2227d859f252d721f2a3c64dc7dc38c32e1e0497
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-02-27 21:07:03 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackagingList />
+      <Stats />>
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackagingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%).</em>
+    </footer>
+  );
+}
