a65191dc6e1a01b97b3c9b906c67dec781fc2eba
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-02-29 21:21:43 +0100

    statistics of items

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -121,10 +121,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list 🌍</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percent = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%).</em>
+      <em>
+        {percent === 100
+          ? "You have everything you need! 🚂🚃🚃"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percent}%) of them.`}
+      </em>
     </footer>
   );
 }
