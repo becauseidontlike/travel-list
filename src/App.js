b351fbdb9d0291d181cccdc1c93e34409bd5be61
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2024-03-01 20:34:03 +0100

    cleaning list

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ export default function App() {
     );
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
@@ -33,6 +40,7 @@ export default function App() {
         items={items}
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -88,7 +96,7 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackagingList({ items, onDeleteItem, onToggleItem }) {
+function PackagingList({ items, onDeleteItem, onToggleItem, onClearList }) {
   const [sortBy, setSortBy] = useState("input");
 
   let sortedItems;
@@ -124,6 +132,7 @@ function PackagingList({ items, onDeleteItem, onToggleItem }) {
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
+        <button onClick={onClearList}>Clear list</button>
       </div>
     </div>
   );
